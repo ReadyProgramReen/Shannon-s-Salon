c56e98b04e134e913c766dff428dd6e1c825fbfc
Maureen E <maureenekeugo1@gmail.com>
2024-09-08 14:27:12 -0400

    Added and styled speciality component

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { specialityData } from "../assets/assets_frontend/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div>SpecialityMenu</div>
-  )
-}
+    <div id="speciality" className="my-20">
+      <h1 className="text-[#bb4537] text-center text-5xl font-bold my-9">
+        Our Services
+      </h1>
+      <hr className="mx-auto w-24 border-t-2 border-primary mb-8" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {specialityData.map((item, index) => (
+          <Link
+            onClick={()=>scrollTo(0,0)}
+            key={index}
+            to={`/stylist/${item.speciality}`}
+            className="relative group"
+          >
+            <img
+              className="w-full h-64 object-cover rounded-lg"
+              src={item.image}
+              alt={item.speciality}
+            />
+            <p className="absolute inset-0 flex items-center justify-center text-white text-xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              {item.speciality}
+            </p>
+            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300 rounded-lg"></div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
