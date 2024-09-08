f9a9a80dd8b4e2300a7381dc1063b43ebcb7ec1f
Maureen E <maureenekeugo1@gmail.com>
2024-09-08 12:12:01 -0400

    Added specialty component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-5 py-10 m-auto md:py-[10vw] md:mb-[-30px]' >
             <p className='text-3xl md:text-4xl lg:text-5xl font-semibold text-yellow-500 leading-tight md:leading-tight lg:leading-tight' >Book Appointments With Our Trusted Stylist </p>
             <p className='text-[#3a4a3b] font-medium mb-5'>Simply Browse through our extensive list of trusted stylist <br /> Schedule your appointments hassle-free</p>
-            <a href="" className='flex items-center gap-2 bg-[#eec4ae]  px-6 py-3 rounded-full text-yellow-500 text-,d font-medium md:m-0 hover:scale-105 hover:font-semibold transition-all duration-300'>Book Appointment <img className='mt-1' src={arrow_icon} alt="" /></a>
+            <a href="#speciality" className='flex items-center gap-2 bg-[#eec4ae]  px-6 py-3 rounded-full text-yellow-500 text-,d font-medium md:m-0 hover:scale-105 hover:font-semibold transition-all duration-300'>Book Appointment <img className='mt-1' src={arrow_icon} alt="" /></a>
         
         </div>
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Home = () => {
   return (
     <div className='mx-4 sm:mx-[5%]'>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
