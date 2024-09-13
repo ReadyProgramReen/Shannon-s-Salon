2c4fd5d76a4466d2c37796cf9c289b18f8579ff9
Maureen E <maureenekeugo1@gmail.com>
2024-09-12 21:10:41 -0400

    Added navigate hook on Show more button

--- frontend/src/components/TopStylist.jsx
+++ frontend/src/components/TopStylist.jsx
@@ -32,7 +32,7 @@ const TopStylist = () => {
           ))}
         </div>
         
-        <button className='mt-6 px-10 py-3 bg-green-500 text-yellow-400 font-medium rounded-full hover:bg-green-600 transition-all'>
+        <button onClick={()=>{navigate('/stylist')}} className='mt-6 px-10 py-3 bg-green-500 text-yellow-400 font-medium rounded-full hover:bg-green-600 transition-all'>
           More
         </button>
       </div>
